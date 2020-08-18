26f0cc89257ba0ea19f6a23ffc03a0ce054ef8e1
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-17 21:21:53 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Home from './components/Home';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hello!!!</h1>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Shop</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            {/* <About /> */}
+          </Route>
+          <Route path="/topics">
+            {/* <Topics /> */}
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
