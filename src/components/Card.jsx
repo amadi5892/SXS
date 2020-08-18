967f34971cba9e4d720dc1cf32150117e8e22b5e
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-17 22:32:10 -0400

    added counter reducer

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
 const Card = (props) => {
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
     return(
         <div className="card">
             {/* Picture */}
             <h3> {props.item} </h3>
             <h4> {props.price} </h4>
             <p> {props.description} </p>
-            <button>+</button>
+            <button onClick={() => dispatch(increment())}>+</button>
             <button>-</button>
         </div>
     )
