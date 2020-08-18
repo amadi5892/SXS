0b8bc5178f36d3d9d3e2612292d3777d50fd2823
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-17 21:25:44 -0400

    add Home.js

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ import {
   useRouteMatch,
   useParams
 } from "react-router-dom";
+import './App.css';
 import Home from './components/Home';
 
 export default function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Shop</Link>
-          </li>
-        </ul>
+        <nav className="nav-bar">
+          <h4>Logo</h4>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Shop</Link>
+            </li>
+          </ul>
+        </nav>
+        
 
         <Switch>
           <Route path="/about">
